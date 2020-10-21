12c6e6ab50db31b960a8f3959cdca015de7c5c38
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-10-21 14:30:20 +0900

    単語を検索するフォームを表示する

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import './App.css';
+
+const validation = Yup.object().shape({
+  word: Yup.string().required("必須項目です"),
+});
 
 function App() {
   return (
     <div className="App">
+      <Formik initialValues={{ word: "" }}
+       validationSchema={validation}
+       onSubmit={values => console.log(values)}
+      >
+        <Form>
+          <label htmlFor="word">word</label>
+          <Field name="word" type="text"/>
+          <ErrorMessage name="word" component="span" style={{color: "red"}} />
+          <button type="submit">送信</button>
+        </Form>
+     </Formik>
     </div>
   );
 }
